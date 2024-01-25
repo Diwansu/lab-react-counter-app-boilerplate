544ba69c15174ec8c9e33331a83f862c3b4701ea
Diwanshu <diwansu531@gmail.com>
2024-01-25 18:17:58 +0530

    Assignment done

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import  {Component}  from "react"
+import './App.css'
+
+export default class App extends Component{
+   constructor(){
+    super() 
+       this.state={count:0,
+      } 
+   }
+        handleReset=()=>{
+          this.setState({count:0}) 
+        }
+
+           Addition = () => {
+          this.setState((prevState) => ({
+          count: prevState.count + 1,
+           }));
+              };
+           
+         Subtraction=()=>{
+          this.setState((prevState)=> ({
+            count: prevState.count - 1 
+          }))
+         }
+
+         render() {
+           return (
+             <div className="resultContainer">
+                 <h2>Counter App</h2>
+                <div className="data">{this.state.count}</div>
+
+                <div className="control">
+                     <button onClick={this.Addition}>+</button>
+                     <button onClick={this.Subtraction}>-</button>
+                     <button onClick={this.handleReset}>Reset</button>
+                </div>
+                
+             </div>
+           )
+         }
+  }
